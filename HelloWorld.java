bcf71f2c8de2df91bca74cf750e0540857264021
Mohamad Afifi <mo7amad3fifi@gmail.com>
2017-01-25 06:28:19 +0200

    Update HelloWorld.java
    fixing errors

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,11 +7,11 @@ public class HelloWorld
       Scanner s = new Scanner(System.in);
       int x = s.nextInt();
       int y = s.nextInt();
-      sum(x,y);
+      System.out.println(sum(x,y));
       System.out.println("Hello,World!");
    }
    
-   public int sum(int x, int y)
+   public static int sum(int x, int y)
    {
       return x + y;
    }
