6b27885acccf63586d756df526af67b78a23cd01
Mohamad Afifi <mo7amad3fifi@gmail.com>
2017-01-25 05:49:49 +0200

    Update HelloWorld.java
    taking input from user

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,19 @@
+import java.util.Scanner; 
+
 public class HelloWorld
 {
    public static void main(String[] args)
    {
+      Scanner s = new Scanner(System.in);
+      int x = s.nextInt();
+      int y = s.nextInt();
+      sum(x,y);
       System.out.println("Hello,World!");
    }
+   
+   public int sum(int x, int y)
+   {
+      return x + y;
+   }
+   
 }
